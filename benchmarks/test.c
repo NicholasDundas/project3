68ce8588e3feb7004b9ab7e8c7a61a9fd4a41561
NicholasDundas <nicholasdundas106@gmail.com>
2024-04-04 09:46:47 -0400

    tests

--- benchmarks/test.c
+++ benchmarks/test.c
@@ -1,23 +1,55 @@
 #include "../my_vm.h"
 #include <stdio.h>
-#define PAGE_SIZE (1<<13)
+
+void print_matrix(unsigned int vp, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            unsigned int value;
+            get_value(vp + (i * cols + j) * sizeof(unsigned int), &value, sizeof(unsigned int));
+            printf("%u ", value);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
 
     set_physical_mem();
     
-    void* tests[7];
-    tests[0] = t_malloc(300*PAGE_SIZE);
-    tests[1] = t_malloc(300*PAGE_SIZE);
-    tests[2] = t_malloc(300*PAGE_SIZE);
-    tests[3] = t_malloc(4096*PAGE_SIZE);
-    tests[4] = t_malloc((1ULL<<30));
-    tests[5] = t_malloc(50);
-    tests[6] = t_malloc(1);
-    for(int i = 0; i < sizeof(tests)/sizeof(tests[0]); i++) {
-        printf("Tests[%d]: %p\n",i,tests[i]);
-        print_va((unsigned int)tests[i]);
+    size_t l = 10, m = 15, n = 10; // Dimensions of the matrices
+
+    // Allocate memory for the matrices
+    unsigned int a = (unsigned int)t_malloc(l * m * sizeof(unsigned int));
+    unsigned int b = (unsigned int)t_malloc(m * n * sizeof(unsigned int));
+    unsigned int c = (unsigned int)t_malloc(l * n * sizeof(unsigned int));
+
+    // Initialize matrix a
+    for (size_t i = 0; i < l * m; ++i) {
+        put_value(a + i * sizeof(unsigned int), &i, sizeof(unsigned int));
     }
+
+    // Initialize matrix b
+    for (size_t i = 0; i < m * n; ++i) {
+        put_value(b + i * sizeof(unsigned int), &i, sizeof(unsigned int));
+    }
+
+    printf("Mat a:\n");
+    print_matrix(a, l, m);
+
+    printf("Mat b:\n");
+    print_matrix(b, m, n);
+    // Multiply
+    mat_mult(a, b, c, l, m, n);
+
+    // Print result
+    printf("Result of matrix multiplication:\n");
+    print_matrix(c, l, n);
+
+    // Free memory
+    t_free(a, l * m * sizeof(unsigned int));
+    t_free(b, m * n * sizeof(unsigned int));
+    t_free(c, l * n * sizeof(unsigned int));
+
     printf("TLB missrate: ");
     print_TLB_missrate();
     printf("\n");
